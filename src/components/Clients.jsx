c2491f87ff354119ca7de4ab13de38a16dbde203
enrique palomino <octepc.97@gmail.com>
2023-03-22 18:29:00 -0500

    feat: :sparkles: Made the last component which render the previous pages

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,16 +1,16 @@
 export const Clients = () => {
   return (
-    <div className="bg-gray-100 p-8 flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-2xl font-medium text-gray-800">
+    <div className="bg-gray-100 p-8 flex flex-col gap-8 items-center justify-center mt-20 xl:mt-0">
+      <h1 className="text-2xl font-medium text-gray-800 text-center">
         Trusted By Greatest Companies
       </h1>
       {/* Logos */}
-      <div className="flex items-center gap-20">
+      <div className="flex flex-col md:flex-row flex-wrap items-center gap-20">
         <img src="google.png" className="w-40" alt="" />
         <img src="shopify.png" className="w-40" alt="" />
         <img src="amazon.png" className="w-40" alt="" />
         <img src="airbnb.png" className="w-40" alt="" />
-        <img src=".png" className="w-40" alt="" />
+        <img src="google.png" className="w-40" alt="" />
       </div>
     </div>
   );
---
+++ src/components/Works.jsx
@@ -0,0 +1,58 @@
+export const Works = () => {
+  return (
+    <div className="p-20">
+      <div>
+        <h1 className="text-[40px] font-bold">
+          We Create World-Class digital products
+        </h1>
+        <p className="text-xl text-gray-500">
+          By information about design the world to the best instructors, heat
+          helping By information
+        </p>
+      </div>
+      {/* Works */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col gap-2">
+          <img src="work1.png" className="w-full rounded-3xl mb-4" alt="" />
+          <p>App Design - June 20, 2022</p>
+          <h3 className="text-2xl font-bold">App Redesign</h3>
+          <p>
+            By information about design the world to the best instructors,
+            helping By information about design the world to the best
+            instructors, helping
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-4">
+            <img src="work2.png" className="w-full rounded-3xl" alt="" />
+            <p>App Design - June 20, 2022</p>
+            <h3 className="text-2xl font-bold">
+              Redesign channel website landing page
+            </h3>
+          </div>
+          <div className="flex flex-col gap-4">
+            <img src="work3.png" className="w-full rounded-3xl" alt="" />
+            <p>App Design - June 20, 2022</p>
+            <h3 className="text-2xl font-bold">
+              New Locator App For a New Company
+            </h3>
+          </div>
+          <div className="flex flex-col gap-4">
+            <img src="work4.png" className="w-full rounded-3xl" alt="" />
+            <p>App Design - June 20, 2022</p>
+            <h3 className="text-2xl font-bold">
+              Rental Rooms Web App Platform
+            </h3>
+          </div>
+          <div className="flex flex-col gap-4">
+            <img src="work5.png" className="w-full rounded-3xl" alt="" />
+            <p>App Design - June 20, 2022</p>
+            <h3 className="text-2xl font-bold">
+              Calendar App for Big SASS Company
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
