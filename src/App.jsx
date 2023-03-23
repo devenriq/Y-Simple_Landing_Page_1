0e0aa614691499dbe4a703ee7c8cf6cf929c28fe
enrique palomino <octepc.97@gmail.com>
2023-03-22 22:00:38 -0500

    feat: :sparkles: Finished the services section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Clients } from "./components/Clients";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Reviews } from "./components/Reviews";
+import { Services } from "./components/Services";
 import { Works } from "./components/Works";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Clients />
       <Works />
       <Reviews />
+      <Services />
     </>
   );
 }
